43d415a06b302945dd45a25b7ea883679afdeb5b
John Peloquin <john.peloquin@gmail.com>
2017-10-12 16:36:39 -0400

    Remove unused variable

--- colormaps.py
+++ colormaps.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.cm
 
-n = 256 # desired number of intensity levels
 cdict_div = {'red': ((0, 0, 0.0941),
                      (0.1, 0.2745, 0.2745),
                      (0.2, 0.4275, 0.4275),
